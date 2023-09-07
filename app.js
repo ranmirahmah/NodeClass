073ff84b8822ea2a95b191c0aa79497c35e36c27
ranmirahmah <ranmigold01@gmail.com>
2023-09-07 16:05:19 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+(function myName() {
+    console.log("my name is Rahmat");
+})(),
+
+(function myName() {
+    console.log("my name is Rahmat");
+})(),
+
+(function myName(a, b) {
+    console.log(a + b);
+})(3,4)
+
